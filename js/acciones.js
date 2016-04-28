4ae9dd08fb8f346ab97b6a2016d14da94d849e41
rojanomartinez <jesus.3fdsaupoo@gmail.com>
2016-04-28 14:57:53 -0500

    6subida

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(e) {
+ document.addEvenListener('deviceready',function() {
+	$('#vibrar').on('tap',function (){
+ navigator.notificacion.vibrate(2000);
+});
+});
+});
+	
